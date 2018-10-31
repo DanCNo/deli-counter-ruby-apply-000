25ef8b4a566cee0bd51e52f0558fbcc1e2313e9a
Dan N <dan.noh@gmail.com>
2018-10-31 18:26:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,21 +9,21 @@ def line(katz_deli)
     idx = 0
     str = "The line is currently:"
     while idx < katz_deli.length
-      str = str + " #{idx+1}. #{katz_deli[idx]}"
+      str = str + " #{katz_deli[idx]}"
       idx += 1 
     end
     puts str 
   end 
 end
 
-def take_a_number(katz_deli, name)
+def take_a_number(katz_deli)
   if katz_deli == []
-    katz_deli << name
-    puts "Welcome, #{name}. You are number 1 in line."
+    katz_deli << 1
+    puts "Welcome. You are number 1 in line."
   
   elsif katz_deli.length > 0
-    katz_deli << name
-    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+    katz_deli << (katz_deli.length + 1) 
+    puts "Welcome. You are number #{katz_deli.length} in line."
   end  
 end
 
